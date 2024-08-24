91ccc6b9f18a3bee508a962f4b80e95bc897ef7f
oferdebug <ofercohen404@gmail.com>
2024-08-24 21:24:45 +0300

    code push

---
+++ app/pages/index.js
@@ -0,0 +1,11 @@
+import styles from 'globals.css';
+
+export default function Home() {
+    return (
+        <div className={styles.container}>
+            <h1 className='text-3xl font-bold underline'>
+            le&rsquo;ts build an ninja-player-by-next
+            </h1>
+        </div>
+    );
+}
---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: false,
+    swcMinify: true,
+    images: {
+      domains: [
+        'res.cloudinary.com',
+        'lh3.googleusercontent.com',
+        'firebasestorage.googleapis.com',
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
